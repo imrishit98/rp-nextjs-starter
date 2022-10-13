cb81fecf1c6262387e9e91e2259ddb05e76762f1
Rishit <imrishit98@gmail.com>
2022-10-13 18:03:21 -0400

    Add GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/main.scss';
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      TagManager.initialize({ gtmId: 'GTM-XXXXXXX' });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
