e2c6053c9662f0a21b8ec55d8972d67f855af4f4
Rishit <imrishit98@gmail.com>
2022-10-13 21:36:08 -0400

    Fix homepage styles

--- src/components/layout/main.js
+++ src/components/layout/main.js
@@ -1,6 +1,6 @@
 const Main = ({ children }) => {
   return (
-    <main className='container w-full flex flex-col items-center justify-center'>
+    <main className='w-full flex flex-col items-center justify-center'>
       {children}
     </main>
   );
