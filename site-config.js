ee06f602804ddf126aa1eec7066af6619f2ed171
Rishit Patel <49016509+imrishit98@users.noreply.github.com>
2022-10-14 23:09:57 -0400

    Add SEO component (#5)

---
+++ site-config.js
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "Rishit's Default Next.js Starter",
+  tagline:
+    'This is a starter template for Next.js projects with Tailwind CSS, SEO, and more.',
+  url: 'https://rp-nextjs-starter.vercel.app',
+  baseUrl: '/',
+  projectName: 'rp-nextjs-starter',
+  organizationName: 'rishitpatel.com',
+};
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,14 @@
 import Footer from './footer';
 import Main from './main';
 import Navbar from './navbar';
+import SEO from './seo';
 
-const Layout = ({ children }) => {
+const Layout = ({ seoTitle, seoDesc, seoPath, seoChildren, children }) => {
   return (
     <>
+      <SEO title={seoTitle} description={seoDesc} path={seoPath}>
+        {seoChildren}
+      </SEO>
       <Navbar />
       <Main>{children}</Main>
       <Footer />
