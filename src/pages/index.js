e2cb98003f9bf084c06a1815eeba56ca978b7099
Rishit <imrishit98@gmail.com>
2022-10-15 21:27:55 -0400

    Fix homepage text alignment

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Container } from '../components/ui/containers';
 const Home = () => {
   return (
     <Layout>
-      <Container className='flex justify-center items-center h-[85vh]'>
+      <Container className='flex justify-center items-center text-center h-[85vh]'>
         <h1 className='text-4xl font-bold'>
           Rishit Patel&apos;s <span className='text-purple-600'>Next.js</span>{' '}
           Starter
