1d507bd505beefd3101b15e251b55f719705c855
Rishit Patel <49016509+imrishit98@users.noreply.github.com>
2022-10-15 23:46:39 -0400

    Change URL of the website (#11)

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ const siteConfig = {
   title: "Rishit's Default Next.js Starter",
   tagline:
     'This is a starter template for Next.js projects with Tailwind CSS, SEO, and more.',
-  url: 'https://rp-nextjs-starter.vercel.app',
+  url: 'https://nextjs-starter.rishit.dev',
   baseUrl: '/',
   projectName: 'rp-nextjs-starter',
   organizationName: 'rishitpatel.com',
