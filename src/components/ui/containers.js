85e9f3f996ce938e2ce26b237da93633e6fb3475
seemaion8 <seema@ion8.net>
2023-04-11 11:37:01 -0400

    Add dropdown functionality to menu

--- src/components/ui/containers.js
+++ src/components/ui/containers.js
@@ -2,7 +2,7 @@ export const Container = ({ className, children }) => {
   return (
     <div
       className={
-        'container w-full px-2 mx-auto md:px-8 lg:px-20 xl:px-40 flex justify-center items-center' +
+        'container w-full px-4 mx-auto md:px-8 lg:px-20 xl:px-40 justify-center items-center' +
         ' ' +
         className
       }
--- src/components/ui/icons.js
+++ src/components/ui/icons.js
@@ -1,3 +1,70 @@
+import React from 'react';
+
+export const CloseIcon = ({ className, ...props }) => {
+  return (
+    <svg
+      width='25'
+      height='25'
+      viewBox='0 0 25 25'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+      {...props}
+    >
+      <path
+        d='M18.8872 6.82495L6.88721 18.825'
+        stroke='#474748'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M6.88721 6.82495L18.8872 18.825'
+        stroke='#474748'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const HamburgerIcon = ({ className, ...props }) => {
+  return (
+    <svg
+      width='25'
+      height='24'
+      viewBox='0 0 25 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className={className}
+      {...props}
+    >
+      <path
+        d='M3.5 12H21.5'
+        stroke='#474748'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3.5 6H21.5'
+        stroke='#474748'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3.5 18H21.5'
+        stroke='#474748'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
 export const SubmitIcon = () => {
   return (
     <svg
