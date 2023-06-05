630d274544c7971b2389918d86192ab385820fb9
seemaion8 <seema@ion8.net>
2023-06-05 15:02:12 -0400

    Install CodeceptJS / add workflow / add first test

---
+++ codecept.conf.js
@@ -0,0 +1,24 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: 'tests/*-test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost:1100',
+      show: true,
+      browser: 'chromium',
+    },
+  },
+  include: {
+    I: './steps_file.js',
+  },
+  name: 'nextjs-starter',
+};
