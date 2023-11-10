7346490bb892339557ed6c07f6935d46f46ac522
Mahesh Singh <mahesh.singh@mail.utoronto.ca>
2023-11-10 02:50:17 -0500

    Update next-sitemap

--- codecept.conf.js
+++ codecept.conf.js
@@ -2,23 +2,43 @@ const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
+require('dotenv').config({ path: './.env.local' });
+const localhostURL = process.env.LOCALHOST_URL || 'http://localhost:3000';
+const { devices } = require('playwright');
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
 setCommonPlugins();
 
-/** @type {CodeceptJS.MainConfig} */
-exports.config = {
+const config = {
   tests: './tests/*-test.js',
   output: './output',
   helpers: {
     Playwright: {
-      url: 'http://localhost:3000',
+      url: localhostURL,
       show: true,
-      browser: 'chromium',
+      browsers: [
+        {
+          browser: 'chromium',
+        },
+        {
+          browser: 'chromium',
+          emulate: devices['iPhone 12'],
+        },
+      ],
     },
+    REST: {
+      prettyPrintJson: true,
+      defaultHeaders: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    },
+    JSONResponse: {},
   },
   include: {
     I: './steps_file.js',
   },
   name: 'nextjs-starter',
 };
+
+module.exports = config;
