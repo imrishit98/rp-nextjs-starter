81fb5463dc4ae39e945d4531a8f77e4eef6ba67f
Mahesh Singh <mahesh.singh@mail.utoronto.ca>
2023-11-10 02:51:24 -0500

    Export site-config

--- site-config.js
+++ site-config.js
@@ -1,11 +1,25 @@
+let siteUrl = '';
+switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+  case 'production':
+    siteUrl = 'https://zrecruit-careers-boilerplate.vercel.app/';
+    break;
+  case 'preview':
+    siteUrl = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    break;
+  case 'development':
+    siteUrl = `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    break;
+}
+
 const siteConfig = {
   title: "ion8's NextJS Starter Template",
   tagline:
     'This is a starter template for Next.js projects with Tailwind CSS, SEO, and more.',
-  url: 'https://github.com/ion8/nextjs-starter',
+  url: siteUrl,
   baseUrl: '/',
   projectName: 'ion8-nextjs-starter',
   organizationName: 'ion8.net',
+  careersRevalidation: 300, // 5 minutes
 };
 
 module.exports = siteConfig;
