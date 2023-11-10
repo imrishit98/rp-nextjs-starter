4384e291781e07ae404c909ed8eea3fd517c7fb2
Mahesh Singh <mahesh.singh@mail.utoronto.ca>
2023-11-10 03:39:31 -0500

    Default headless true

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,7 +1,7 @@
 const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+setHeadlessWhen(process.env.HEADLESS || true);
 const localhostURL = process.env.LOCALHOST_URL || 'http://localhost:3000';
 const { devices } = require('playwright');
 
