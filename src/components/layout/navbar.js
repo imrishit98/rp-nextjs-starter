657e5cd58711130a0a1f05b73f6049b441789b46
Rishit <imrishit98@gmail.com>
2024-01-30 11:26:25 -0500

    Add external links to navbar

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -4,9 +4,9 @@ import { Container } from '@/components/ui/containers';
 
 const Navbar = () => {
   const navLinks = [
-    { href: '/', text: 'Home' },
-    { href: '/blog', text: 'Blog' },
     { href: '/about', text: 'About' },
+    { href: 'https://aitools.fyi/', text: 'aitools.fyi' },
+    { href: 'https://amazingnewsletters.com/', text: 'Amazing Newsletters' },
   ];
 
   return (
@@ -24,6 +24,9 @@ const Navbar = () => {
               key={index}
               href={link.href}
               className='px-4 py-2 text-sm font-medium hover:font-bold'
+              {...(link.href.startsWith('http')
+                ? { target: '_blank', rel: 'noopener noreferrer' }
+                : {})}
             >
               {link.text}
             </Link>
