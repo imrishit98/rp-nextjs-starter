26d9ced833c9870c4b85fa1b7ab68ca73012ff5a
Rishit <imrishit98@gmail.com>
2024-01-30 11:22:52 -0500

    Refactor navbar component to use dynamic navigation links

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -2,36 +2,31 @@ import Link from 'next/link';
 import { Container } from '../ui/containers';
 
 const Navbar = () => {
+  const navLinks = [
+    { href: '/', text: 'Home' },
+    { href: '/blog', text: 'Blog' },
+    { href: '/about', text: 'About' },
+  ];
+
   return (
     <nav className='py-4 border-b-2'>
       <Container className='flex justify-between items-center'>
-        <div className='flex items-center '>
-          <Link
-            href='/'
-            className='text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300 lg:text-3xl'
-          >
-            RP
-          </Link>
-        </div>
+        <Link
+          href='/'
+          className='text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300 lg:text-3xl'
+        >
+          RP
+        </Link>
         <div className='flex'>
-          <Link
-            href='/'
-            className='px-4 py-2 text-sm font-medium hover:font-bold'
-          >
-            Home
-          </Link>
-          <Link
-            href='/blog'
-            className='px-4 py-2 text-sm font-medium hover:font-bold'
-          >
-            Blog
-          </Link>
-          <Link
-            href='/about'
-            className='px-4 py-2 text-sm font-medium hover:font-bold'
-          >
-            About
-          </Link>
+          {navLinks.map((link, index) => (
+            <Link
+              key={index}
+              href={link.href}
+              className='px-4 py-2 text-sm font-medium hover:font-bold'
+            >
+              {link.text}
+            </Link>
+          ))}
         </div>
       </Container>
     </nav>
