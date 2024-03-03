554979b753bab8c9563b0e4b2a9ee88cd2cf9a1c
Rishit <imrishit98@gmail.com>
2024-03-03 18:51:46 -0500

    Add menu and close icons

---
+++ src/components/ui/icons/menus.js
@@ -0,0 +1,31 @@
+export const MenuIcon = () => {
+  return (
+    <svg
+      className='w-6 h-6'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+    >
+      <path d='M4 6h16M4 12h16m-7 6h7' />
+    </svg>
+  );
+};
+
+export const CloseIcon = () => {
+  return (
+    <svg
+      className='w-6 h-6'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='2'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+    >
+      <path d='M6 18L18 6M6 6l12 12' />
+    </svg>
+  );
+};
