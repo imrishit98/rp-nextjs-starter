e2448753de302f79a4082ba73c8644936d05a0f5
Rishit <imrishit98@gmail.com>
2024-09-14 13:24:55 -0400

    feat: Update page.js to use H1 component for the title

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
+import { H1 } from '@/components/ui/typography';
 import { Container } from '../components/ui/containers';
 
 const Home = () => {
   return (
     <Container className='flex justify-center items-center text-center h-[85vh]'>
-      <h1 className='text-4xl font-bold'>
-        Rishit Patel&apos;s <span className='text-purple-600'>Next.js</span>{' '}
-        Starter
-      </h1>
+      <H1>
+        Rishit Patel&apos;s <span className='text-purple-600'>Next.js</span> Starter
+      </H1>
     </Container>
   );
 };
