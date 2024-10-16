ca2f660f5f85ea54a30c9bc33b235fd9d66b460b
Rishit <imrishit98@gmail.com>
2024-10-16 14:30:08 -0400

    Refactor: Update robots.js to use destructuring for importing siteConfig

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,18 +1,20 @@
-import siteConfig from '../../site-config';
+import { url } from '@/../../site-config';
 
+/**
+ * Generates the robots.txt configuration for the application.
+ * @returns {Object} - The robots configuration object containing rules, sitemap, and host.
+ */
 const robots = () => {
-  const { url } = siteConfig;
-
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/private/', '/api/'],
+        userAgent: '*', // Applies to all user agents
+        allow: '/', // Allows access to the root directory
+        disallow: ['/private/', '/api/'], // Disallows access to specific directories
       },
     ],
-    sitemap: `${url}/sitemap.xml`,
-    host: url,
+    sitemap: `${url}/sitemap.xml`, // URL to the sitemap
+    host: url, // Host URL of the application
   };
 };
 
