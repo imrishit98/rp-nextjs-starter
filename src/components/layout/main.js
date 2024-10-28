4f3530c33bd5bb092394c000f302ea6f538eed43
Rishit <imrishit98@gmail.com>
2024-10-28 15:34:26 -0400

    Refactor: Add documentation to main.js component

--- src/components/layout/main.js
+++ src/components/layout/main.js
@@ -1,3 +1,11 @@
+/**
+ * Main content wrapper component.
+ * Centers content horizontally and vertically within the page.
+ * @component
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - Content to be rendered within the main section
+ * @returns {JSX.Element} Main content wrapper
+ */
 const Main = ({ children }) => {
   return (
     <main className='w-full flex flex-col items-center justify-center'>
