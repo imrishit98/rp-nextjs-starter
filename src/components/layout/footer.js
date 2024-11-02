b1d13a4a32fb6317e402d4a1fa9d2bf88d26a40b
Rishit <imrishit98@gmail.com>
2024-11-01 23:55:12 -0400

    Refactor: Update import path for site name in footer.js

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 import { Container } from '@/components/ui/containers';
+import { siteName } from '../../../site-config';
 
 /**
  * Footer component that displays copyright information and a link.
@@ -13,8 +14,11 @@ const Footer = () => {
     <footer className='py-2 border-t-2'>
       <Container className='flex justify-center items-center text-sm font-medium'>
         © {new Date().getFullYear()}&nbsp;
-        <Link href={'/'} target='_blank' rel='noopener noreferrer'>
-          Rishit Patel
+        <Link
+          href={'/'}
+          target='_blank'
+          rel='noopener noreferrer'>
+          {siteName}
         </Link>
         . All Rights Reserved.
       </Container>
