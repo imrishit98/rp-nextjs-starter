d5e5c34d7216130ac1ba0e971382fa72ab9d4233
Rishit <imrishit98@gmail.com>
2024-11-01 23:53:00 -0400

    Refactor: Update site name in navbar.js

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,6 +1,7 @@
 import { Container } from '@/components/ui/containers';
 import Link from 'next/link';
 import NavMenu from './menu';
+import { siteName } from '../../../site-config';
 
 /**
  * Navigation bar component that appears at the top of every page.
@@ -14,9 +15,8 @@ const Navbar = () => {
       <Container className='flex justify-between items-center'>
         <Link
           href='/'
-          className='text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300 lg:text-3xl'
-        >
-          RP
+          className='text-2xl font-bold text-gray-800 hover:text-gray-700 dark:text-white dark:hover:text-gray-300 lg:text-3xl'>
+          {siteName}
         </Link>
         <NavMenu />
       </Container>
